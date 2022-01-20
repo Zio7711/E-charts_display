702045f33e854ece0fcb36964369d13c55b270ac
zio7711 <zi.ao.tan@outlook.com>
2022-01-20 11:57:40 +0800

    create and register route for trendpage

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import SellerPage from "../views/SellerPage.vue";
+import TrendPage from "@/views/TrendPage";
 
 Vue.use(VueRouter);
 
-const routes = [{ path: "/sellerpage", component: SellerPage }];
+const routes = [
+  { path: "/sellerpage", component: SellerPage },
+  { path: "/trendpage", component: TrendPage },
+];
 
 const router = new VueRouter({
   routes,
