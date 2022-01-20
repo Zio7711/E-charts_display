1170ea87def1baf897a0ce9c6851b7bb03c8475b
zio7711 <zi.ao.tan@outlook.com>
2022-01-20 14:44:51 +0800

    change area style for line chart

--- src/components/report/Trend.vue
+++ src/components/report/Trend.vue
@@ -26,11 +26,29 @@ export default {
         "purple-passion"
       );
       const initOption = {
+        grid: {
+          left: "3%",
+          top: "35%",
+          right: "4%",
+          bottom: "1%",
+          containLabel: true,
+        },
+        tooltip: {
+          // 当鼠标移入坐标轴的显示提示
+          trigger: "axis",
+        },
         xAxis: {
           type: "category",
+          boundaryGap: false,
         },
         yAxis: {
           type: "value",
+        },
+
+        legend: {
+          left: "center",
+          top: "18%",
+          icon: "circle",
         },
       };
       this.chartInstance.setOption(initOption);
@@ -44,6 +62,22 @@ export default {
     },
 
     updateChart() {
+      // 半透明的颜色值
+      const colorArr1 = [
+        "rgba(11, 168, 44, 0.5)",
+        "rgba(44, 110, 255, 0.5)",
+        "rgba(22, 242, 217, 0.5)",
+        "rgba(254, 33, 30, 0.5)",
+        "rgba(250, 105, 0, 0.5)",
+      ];
+      // 全透明的颜色值
+      const colorArr2 = [
+        "rgba(11, 168, 44, 0)",
+        "rgba(44, 110, 255, 0)",
+        "rgba(22, 242, 217, 0)",
+        "rgba(254, 33, 30, 0)",
+        "rgba(250, 105, 0, 0)",
+      ];
       const timeArr = this.allData.common.month;
       const valueArr = this.allData.map.data;
       const seriesArr = valueArr.map((item, index) => {
@@ -52,6 +86,18 @@ export default {
           type: "line",
           data: item.data,
           stack: "map",
+          areaStyle: {
+            color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              {
+                offset: 0,
+                color: colorArr1[index],
+              },
+              {
+                offset: 1,
+                color: colorArr2[index],
+              },
+            ]),
+          },
         };
       });
 
