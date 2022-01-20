03e74c35a47299fc37613dd57aa1a1246966b5c7
zio7711 <zi.ao.tan@outlook.com>
2022-01-20 18:26:35 +0800

    create and register map and mappage

---
+++ src/components/report/Map.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="com-container">
+    <div class="com-chart" ref="trendRef"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Map",
+
+  data() {
+    return {
+      chartInstance: null,
+      allData: null,
+      showMenu: false,
+    };
+  },
+
+  methods: {
+    initChart() {
+      this.chartInstance = this.$echarts.init(
+        this.$refs.trendRef,
+        "purple-passion"
+      );
+      const initOption = {};
+      this.chartInstance.setOption(initOption);
+    },
+
+    async getData() {
+      const { data: res } = await this.$http.get("/api/trend");
+      this.allData = res;
+
+      this.updateChart();
+    },
+
+    updateChart() {
+      const dataOption = {};
+      this.chartInstance.setOption(dataOption);
+    },
+
+    screenAdapter() {
+      const adapterOption = {};
+      this.chartInstance.setOption(adapterOption);
+      this.chartInstance.resize();
+    },
+  },
+
+  mounted() {
+    this.initChart();
+    this.getData();
+    window.addEventListener("resize", this.screenAdapter);
+    this.screenAdapter();
+  },
+
+  destroyed() {
+    window.removeEventListener("resize", this.screenAdapter);
+  },
+};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import SellerPage from "../views/SellerPage.vue";
 import TrendPage from "@/views/TrendPage";
+import MapPage from "@/views/MapPage";
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/sellerpage", component: SellerPage },
   { path: "/trendpage", component: TrendPage },
+  { path: "/mappage", component: MapPage },
 ];
 
 const router = new VueRouter({
