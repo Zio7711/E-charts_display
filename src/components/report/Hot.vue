b9d65019db0557f19d2febe38ff11a361fc3623c
zio7711 <zi.ao.tan@outlook.com>
2022-01-21 14:44:02 +0800

    display pie chart basic data

--- src/components/report/Hot.vue
+++ src/components/report/Hot.vue
@@ -9,7 +9,12 @@ export default {
   name: "Hot",
 
   data() {
-    return {};
+    return {
+      chartInstance: null,
+      allData: null,
+      currentIndex: 0,
+      titleFontSize: null,
+    };
   },
 
   methods: {
@@ -18,19 +23,43 @@ export default {
         this.$refs.hotRef,
         "purple-passion"
       );
-      const initOption = {};
+      const initOption = {
+        series: [
+          {
+            type: "pie",
+          },
+        ],
+      };
       this.chartInstance.setOption(initOption);
     },
 
     async getData() {
-      const { data: res } = await this.$http.get("/api/trend");
+      const { data: res } = await this.$http.get("/hotproduct");
       this.allData = res;
-
       this.updateChart();
     },
 
     updateChart() {
-      const dataOption = {};
+      const legendDateArr = this.allData[this.currentIndex].children.map(
+        (item) => item.name
+      );
+      const seriesDataArr = this.allData[this.currentIndex].children.map(
+        (item) => {
+          return {
+            name: item.name,
+            value: item.value,
+            children: item.children,
+          };
+        }
+      );
+
+      const dataOption = {
+        series: [
+          {
+            data: seriesDataArr,
+          },
+        ],
+      };
       this.chartInstance.setOption(dataOption);
     },
 
