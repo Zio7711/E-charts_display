734f030498b5daaa57703c188af1d7ed434a58cc
zio7711 <zi.ao.tan@outlook.com>
2022-01-21 15:57:49 +0800

    config ui style for pie chart

--- src/components/report/Stock.vue
+++ src/components/report/Stock.vue
@@ -12,10 +12,22 @@ export default {
     return {
       chartInstance: null,
       allData: null,
-      showMenu: false,
-      activeName: "map",
-      titleFontSize: 0,
-      value: "",
+      currentIndex: 1,
+      timerId: null,
+      centerArr: [
+        ["18%", "40%"],
+        ["50%", "40%"],
+        ["82%", "40%"],
+        ["34%", "75%"],
+        ["66%", "75%"],
+      ],
+      colorArr: [
+        ["#4FF778", "#0BA82C"],
+        ["#E5DD45", "#E8B11C"],
+        ["#E8821C", "#E55445"],
+        ["#5052EE", "#AB6EE5"],
+        ["#23E5E5", "#2E72BF"],
+      ],
     };
   },
 
@@ -25,19 +37,75 @@ export default {
         this.$refs.stockRef,
         "purple-passion"
       );
-      const initOption = {};
+      const initOption = {
+        title: {
+          text: "▎库存和销量分析",
+          left: 20,
+          top: "5%",
+        },
+      };
       this.chartInstance.setOption(initOption);
     },
 
     async getData() {
-      const { data: res } = await this.$http.get("/api/stock");
+      const { data: res } = await this.$http.get("stock");
       this.allData = res;
 
       this.updateChart();
     },
 
     updateChart() {
-      const dataOption = {};
+      const start = (this.currentIndex - 1) * 5;
+      const end = start + 5;
+      const showData = this.allData.slice(start, end);
+
+      let seriesArr = showData.map((item, index) => {
+        return {
+          type: "pie",
+          hoverAnimation: false,
+          labelLine: {
+            show: false,
+          },
+          radius: [110, 100],
+          center: this.centerArr[index],
+          label: { position: "center", color: this.colorArr[index][0] },
+          data: [
+            {
+              name: item.name + "\n\n" + item.sales,
+              value: item.sales,
+
+              itemStyle: {
+                // 创建线性渐变的颜色 从下往上
+                color: new this.$echarts.graphic.LinearGradient(0, 1, 0, 0, [
+                  // 0%
+                  { offset: 0, color: this.colorArr[index][0] },
+                  // 100%
+                  { offset: 1, color: this.colorArr[index][1] },
+                ]),
+              },
+
+              tooltip: {
+                formatter: `${item.name} <br/>销量: {c} <br/>占比: {d}%`,
+              },
+            },
+            {
+              value: item.stock,
+              itemStyle: {
+                color: "#bbb",
+              },
+
+              tooltip: {
+                formatter: `${item.name} <br/>库存: {c} <br>占: {d}%`,
+              },
+            },
+          ],
+        };
+      });
+
+      const dataOption = {
+        series: seriesArr,
+      };
+
       this.chartInstance.setOption(dataOption);
     },
 
